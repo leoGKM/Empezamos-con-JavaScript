4e6855834189de7235b3c30df741325f659037ab
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-03-03 20:33:19 -0400

    multiplicador2

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,11 @@
-import sumar from "./sumador";
 import multiplicar from "./multiplicador";
+import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const multiplyButton = document.querySelector("#multiplicar-button");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -14,11 +15,9 @@ form.addEventListener("submit", (event) => {
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
 
+multiplyButton.addEventListener("click", () => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) +"</p>";
 });
