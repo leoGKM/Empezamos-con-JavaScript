0f95691315720c1ebf584ffe8415a772e6c6e5cd
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-03-04 10:55:50 -0400

    feat: saludar

--- src/presenter.js
+++ src/presenter.js
@@ -1,23 +1,9 @@
-import multiplicar from "./multiplicador";
-import sumar from "./sumador";
-
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+import saludar from "./saludador.js";
+ 
+const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
-const multiplyButton = document.querySelector("#multiplicar-button");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
-
-multiplyButton.addEventListener("click", () => {
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) +"</p>";
+  div.innerHTML = "<p>" + saludar() + ".</p>";
 });
